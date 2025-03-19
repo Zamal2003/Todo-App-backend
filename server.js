c6701772a989ce0ccb2c26606f397fca6ad01cf5
Md Jamaluddin <mdjamaluddina007@gmail.com>
2025-03-19 14:09:39 +0530

    first commit

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "https://todo-app-my-app.vercel.app", // Replace with your frontend URL
+    origin: ["https://todo-app-my-app.vercel.app" ,'*'], // Replace with your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+   credentials:true
   })
 );
 app.use(express.json());
