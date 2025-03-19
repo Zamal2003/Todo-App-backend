d844f53bd4a0ac79f3c4c601195a712790d52860
Md Jamaluddin <mdjamaluddina007@gmail.com>
2025-03-19 13:55:03 +0530

    first commit

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ console.log('MONGO_URI:', process.env.MONGO_URI);
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://todo-app-my-app.vercel.app", // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 app.use(express.json());
 
 // MongoDB connection
